bc5601afe3fcadbe8351c4951fb5476d0ce91da5
ryungeon <ekdlwp123@gmail.com>
2022-09-01 17:52:07 +0900

    proxy & axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
 
 import './App.css';
+import LandingPage from './component/views/LandingPage/LandingPage';
+import LoginPage from './component/views/LoginPage/LoginPage';
+import RegisterPage from './component/views/RegisterPage/RegisterPage';
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<LandingPage />} />
+          <Route exact path="/loginpage" element={<LoginPage />} />
+          <Route exact path="/registerpage" element={<RegisterPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -27,6 +27,8 @@ mongoose.connect(config.mongoURI, {
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
+app.get('/api/hello', (req, res) => res.send('Hello World!~~ '))
+
 app.post('/api/users/register', (req, res) => {
     //회원가입 할 때 필요한 정보들을 client에서 가져오면 그것들을 데이터 베이스에 넣어준다. 
 
